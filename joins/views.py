cefd149eb32548d87006ff5004d185989de1f354
alex <t0983781367@gmail.com>
2016-03-29 15:54:16 +0300

    add file

---
+++ joins/views.py
@@ -0,0 +1,57 @@
+from django.shortcuts import render, HttpResponseRedirect
+from .forms import EmailForm, JoinForm
+from .models import Join
+
+def get_ip(request):
+    try:
+        x_forward = request.META.get("HTTP_X_FORWARDED_FOR")
+        if x_forward:
+            ip = x_forward.split(",")[0]
+        else:
+            ip = request.META.get("REMOTE_ADDR")
+    except:
+        ip = ""
+    return ip
+import uuid
+def get_ref_id():
+    ref_id = str(uuid.uuid4())[:11].replace('-', '').lower()
+    try:
+        id_exists = Join.objects.get(ref_id = ref_id)
+        get_ref_id()
+    except:
+        return ref_id
+
+def share(request, ref_id):
+    # print ref_id
+    context = {'ref_id': ref_id}
+    template = 'share.html'
+    return render(request, template, context)
+
+def home(request):
+    try:
+        refer_id = request.session['join_id_ref']
+        obj = Join.objects.get(id = refer_id)
+    except:
+       obj = None
+
+    form = JoinForm(request.POST or None)
+    if  form.is_valid():
+        new_join = form.save(commit=False)
+        email = form.cleaned_data['email']
+        new_join_old, created = Join.objects.get_or_create(email = email)
+        if created:
+            new_join_old.ref_id = get_ref_id()
+            #   add our friend  who  reffered  us to our join model or a related
+            if not obj ==None:
+                new_join_old.ref_id = get_ref_id()
+            new_join_old.ip_address = get_ip(request)
+            new_join_old.save()
+
+       #print all 'friends' that joined as a result of main share email
+            print Join.objects.filter(friend = obj)
+            print  obj.referral.all()
+
+        return HttpResponseRedirect("/%s" % (new_join_old.ref_id))
+    context = {"form": form}
+    template = 'home.html'
+    return render(request, template, context)
